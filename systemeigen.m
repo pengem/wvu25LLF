b615962a25ddd61a873a89ea81f7e044c7cd130f
pengem <85187423+pengem@users.noreply.github.com>
2025-06-17 14:03:25 -0400

    Update systemeigen.m
    added some comments

--- systemeigen.m
+++ systemeigen.m
@@ -7,7 +7,10 @@ function [lambdamax] = systemeigen(u,p,pbar,aexp,A)
 lambda1 = -(u + A) .* ((1 + aexp) .* (p ./ pbar).^aexp - 1);
 lambda2 = -(u + A) .* ((p ./ pbar).^aexp - 1);
 
+% basically lambda1 = [lambda_1^L, lambda_1^R]
+% and       lambda2 = [lambda_2^L, lambda_2^R]
 
+%lambdamax is used for CFL condition
 lambdamax = max(max(abs(lambda1), abs(lambda2)));
 
 end
