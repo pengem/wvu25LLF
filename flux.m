a3e6e83ca921ccd540008cdec61ec1a013382984
ehallor <ehallor@iu.edu>
2025-10-16 15:34:03 -0400

    Update flux.m

--- flux.m
+++ flux.m
@@ -2,9 +2,9 @@
 % RETURNS: f1 and f2 as [f1, f2]
 % note that A must be calculated outside of the function (as of now)
 
-function[f1, f2] = flux(u,p,pbar,aexp, a_s, t)
+function[f1, f2] = flux(u,p,pbar,aexp, amp, freq, t)
 
-f1 = p.*u - p.*u.*((p/pbar).^aexp) + p.*integral(a_s, 0, t) - p.*integral(a_s, 0, t).*((p/pbar).^aexp);
+f1 = p.*u - p.*u.*((p/pbar).^aexp) + p.*amp/freq*sin(freq*t) - p.*amp/freq*sin(freq*t).*((p/pbar).^aexp);
 
-f2 = p.*(u.^2) - p.*(u.^2).*((p/pbar).^aexp) + p.*u.*integral(a_s, 0, t) - p.*u.*integral(a_s, 0, t).*((p/pbar).^aexp);
+f2 = p.*(u.^2) - p.*(u.^2).*((p/pbar).^aexp) + p.*u.*amp/freq*sin(freq*t) - p.*u.*amp/freq*sin(freq*t).*((p/pbar).^aexp);
 end
