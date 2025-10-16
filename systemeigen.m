e81ade04838c42ab5be303647579ea56f00bdce4
ehallor <ehallor@iu.edu>
2025-10-16 15:35:40 -0400

    Update systemeigen.m

--- systemeigen.m
+++ systemeigen.m
@@ -1,11 +1,11 @@
-function [lambdamax] = systemeigen(u,p,pbar,aexp, a_s, t)
+function [lambdamax] = systemeigen(u,p,pbar,aexp, amp, freq, t)
 % Computes the local system eigenvalues for specified 
 % time and characteristics and returns the max
 % Matlab will automatically discard imaginary parts
 % RETURNS: max of absolute value of lambda1 and lambda2
 
-lambda1 = -(u + integral(a_s, 0, t)) .* ((1 + aexp) .* (p ./ pbar).^aexp - 1);
-lambda2 = -(u + integral(a_s, 0, t)) .* ((p ./ pbar).^aexp - 1);
+lambda1 = -(u + amp/freq*sin(freq*t)) .* ((1 + aexp) .* (p ./ pbar).^aexp - 1);
+lambda2 = -(u + amp/freq*sin(freq*t)) .* ((p ./ pbar).^aexp - 1);
 
 % basically lambda1 = [lambda_1^L, lambda_1^R]
 % and       lambda2 = [lambda_2^L, lambda_2^R]
